3d3d3a00efc8266a9df669bce3001c2480c7ee52
TheKebabo <gabrielaltwargpeel@gmail.com>
2025-01-30 13:37:55 +0000

    started some lin alg transformations

--- main.cpp
+++ main.cpp
@@ -3,7 +3,11 @@
 #include <vector>
 #include <glad/glad.h> 
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include "shader.h"
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
@@ -75,6 +79,16 @@ int main()
     // Set each uniform sampler to the correct texture unit
     mainShader->setInt("texture1", 0);
     mainShader->setInt("texture2", 1);
+
+    // OBJECT TRANSFORMATIONS
+    // ----------------------
+    glm::mat4 trans = glm::mat4(1.0f);
+    trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
+    trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));  
+
+    // Set uniform in shader program
+    unsigned transformLoc = glGetUniformLocation(mainShader->ID, "transform");
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
     // RENDER LOOP
     // -----------
--- src/vertexShader.vs
+++ src/vertexShader.vs
@@ -1,15 +1,14 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec3 aCol;
 layout (location = 2) in vec2 aTexCoord;
 
-// Both passed to fragment shader
-out vec3 colToFrag;
+// Passed to fragment shader
 out vec2 texCoordToFrag;
+
+uniform mat4 transform;
 
 void main()
 {
-    gl_Position = vec4(aPos, 1.0);
-    colToFrag = aCol;
+    gl_Position = transform * vec4(aPos, 1.0f);
     texCoordToFrag = aTexCoord;
 }
